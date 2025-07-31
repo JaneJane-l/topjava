4beded72d5559da89136601b6cde17b495d89404
cat <evgeniya-l@bk.ru>
2025-07-31 07:48:20 +0300

    3 15 fix servlet

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_role;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -10,3 +12,8 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, calories)
+VALUES ((SELECT id from users where name = 'User'), 'Ужин', 500 ),
+       ((SELECT id from users where name = 'Admin'), 'Обед', 450),
+       ((SELECT id from users where name = 'Guest'),'Завтрак', 300);
